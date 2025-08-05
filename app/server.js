9ee381d3d26cfbc1c560f94e1a5fc5d342fc40cf
Romain Liénard <rhum1@MacBook-Air-de-Romain.local>
2025-08-05 12:24:23 +0200

    fix root

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const { decideAction } = require('./bot');
+const { decideAction } = require('./bot/bot');
 
 app.use(cors());
 
